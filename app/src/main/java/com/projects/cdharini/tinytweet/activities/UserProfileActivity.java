082981ab9d4798d192ee4541672805fb6580d1c7
Chandrasekaran <dharini.chandrasekaran@gmail.com>
2017-10-07 10:13:15 -0700

    More details in Profile View
    User can view # of followers, # of following
    and taglines in the profile view along with profile image.

--- app/src/main/java/com/projects/cdharini/tinytweet/activities/UserProfileActivity.java
+++ app/src/main/java/com/projects/cdharini/tinytweet/activities/UserProfileActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.projects.cdharini.tinytweet.R;
 import com.projects.cdharini.tinytweet.fragments.TimelineFragment;
 import com.projects.cdharini.tinytweet.models.User;
 import com.projects.cdharini.tinytweet.utils.TinyTweetConstants;
 
 import org.parceler.Parcels;
+
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 
 public class UserProfileActivity extends AppCompatActivity {
@@ -34,8 +39,23 @@ public class UserProfileActivity extends AppCompatActivity {
             ft.commit();
         }
 
-        //get views
-        //set the values there
+        TextView tvUserName = (TextView) findViewById(R.id.tvUserName);
+        TextView tvScreenName = (TextView) findViewById(R.id.tvScreenName);
+        TextView tvTagline = (TextView) findViewById(R.id.tvTagline);
+        TextView tvFollowers = (TextView) findViewById(R.id.tvNumFollowers);
+        TextView tvFollowing = (TextView) findViewById(R.id.tvNumFollowing);
+        ImageView ivProfile = (ImageView) findViewById(R.id.ivProfilePic);
+
+        tvUserName.setText(mUser.getName());
+        tvScreenName.setText("@" + mUser.getScreenName());
+        tvFollowers.setText(String.valueOf(mUser.getFollowersCount()));
+        tvFollowing.setText(String.valueOf(mUser.getFollowingCount()));
+        tvTagline.setText(mUser.getDescription());
+
+
+        Glide.with(this).load(mUser.getProfilePicUrl())
+                .bitmapTransform(new CropCircleTransformation(this))
+                .error(R.drawable.ic_error_outline_black_24dp).into(ivProfile);
     }
 
 }
